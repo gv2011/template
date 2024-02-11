6002a7dbd8de0fac5781aff727266f57d9af858e
Vinz <gv2011@users.noreply.github.com>
2024-02-11 17:25:14 +0100

    hash

--- src/main/java/com/github/gv2011/template/Main.java
+++ src/main/java/com/github/gv2011/template/Main.java
@@ -7,12 +7,12 @@ import org.slf4j.Logger;
 
 public class Main {
 
-	private static final Logger LOG = getLogger(Main.class);
+  private static final Logger LOG = getLogger(Main.class);
 
-	public static void main(final String[] args) {
-		final String msg = "¡Hola!";
-		System.out.println(msg);
-		LOG.info("Printed \"{}\".", msg);
-	}
+  public static void main(final String[] args) {
+    final String msg = "¡Hola"+(args.length>0 ? " "+args[0] : "")+"!";
+    System.out.println(msg);
+    LOG.info("Printed \"{}\".", msg);
+  }
 
 }
